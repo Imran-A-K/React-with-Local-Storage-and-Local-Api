d46f1b9d962d25de870e877838a48e481741f081
Imran-A-K <imran.k.professional@gmail.com>
2023-03-25 22:53:24 +0600

    third

--- src/Components/Cosmetics_Component/Cosmetic.jsx
+++ src/Components/Cosmetics_Component/Cosmetic.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import { addToDb } from '../../utilities/fakedb'
 import "./Cosmetic.css"
 
 const Cosmetic = (props) => {
     const {name,price,id} = props.cosmetic
-    const addToCart = () =>{
-        
+    const addToCart = (id) =>{
+    addToDb(id)
     }
+    const addToCartWithParameter =() => addToCart(id)
   return (
     <div className='product'>
         <h2>Buy this: {name} </h2>
         <p>Only for: ${price}</p>
         <p><small>it has id: {id}</small></p>
-        <button onClick={addToCart}>Add to cart</button>
+        {/* <button onClick={addToCartWithParameter}>Add to cart</button> */}
+{/* or */}
+        <button onClick={() =>addToCart(id)}>Add to cart</button>
     </div>
   )
 }
